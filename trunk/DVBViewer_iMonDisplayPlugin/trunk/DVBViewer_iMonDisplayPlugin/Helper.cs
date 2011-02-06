9d57760b49490ba7cdd4815958b130e74daebaad
florian.halfmann@googlemail.com <florian.halfmann@googlemail.com@80b59031-84f0-300a-a167-7b7b6b74e8ae>
2011-02-06 16:26:15 +0000

    v0.5.2.0 Release - fixed VFD problem with german special chars, fixed crash if close graph in dvbviewer

---
+++ trunk/DVBViewer_iMonDisplayPlugin/trunk/DVBViewer_iMonDisplayPlugin/Helper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DVBViewer_iMonDisplayPlugin
+{
+    class Helper
+    {
+        public static string ReplaceGermanSpecialCharsForVFD(string input)
+        {
+            string result = String.Empty;
+            result = input.Replace("ä", "ae");
+            result = result.Replace("ö", "oe");
+            result = result.Replace("ü", "ue");
+            result = result.Replace("ß", "ss");
+            return result;
+        }
+    }
+}
